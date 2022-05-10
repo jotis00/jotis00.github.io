8024d85d49c35420a3ff769b5f18baa879c2bae7
Jaxson Otis <jotisbsb@outlook.com>
2022-05-10 12:23:47 -0700

    m

---
+++ index.js
@@ -0,0 +1,22 @@
+const project1Div = document.getElementById("project1")
+const project2Div = document.getElementById("project2")
+const project3Div = document.getElementById("project3")
+const project4Div = document.getElementById("project4")
+
+
+project1Div.addEventListener('click', () => {
+    window.open('https://gtrk.herokuapp.com/', '_blank'); 
+})
+
+project1Div.addEventListener('click', () => {
+    window.location.href = "";
+})
+
+project1Div.addEventListener('click', () => {
+    window.location.href = "";
+})
+
+project1Div.addEventListener('click', () => {
+    window.location.href = "";
+})
+
